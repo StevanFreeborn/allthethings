bdc8b5e4981f90361a9d8078e489ff85e042cbc6
StevanFreeborn <sfreeborn7@gmail.com>
2022-06-04 16:44:40 -0500

    add bootstrap icons, added icon to navbar brand

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { CheckSquare } from 'react-bootstrap-icons';
 
 export default function NavBar() {
 
@@ -8,11 +9,13 @@ export default function NavBar() {
         <div>
             <nav className='navbar navbar-dark bg-dark mb-5'>
 
-                <NavLink className='navbar-brand mx-5' to='/'>
-                    All The Things
+                <NavLink className='navbar-brand mx-4' to='/'>
+                    <div style={{ display: "flex", alignItems: 'center' }}>
+                        <CheckSquare /> <span className='ms-2'>All The Things</span>
+                    </div>
                 </NavLink>
 
-                <div className='nav mx-5'>
+                <div className='nav mx-2'>
 
                     <NavLink className='nav-link link-light' to='/tasks'>
                         Tasks
