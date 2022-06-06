d28b7d563ca003f6a19fa9022447deb282bc4526
StevanFreeborn <sfreeborn7@gmail.com>
2022-06-06 12:02:11 -0500

    moved request handling logic from tasks routes to task controller.

---
+++ controllers/tasks.js
@@ -0,0 +1,69 @@
+const Task = require('../models/task');
+
+class TasksController {
+
+    getAllTasks = async (req, res) => {
+
+        const tasks = await Task.find({}).exec().catch(err => console.log(err));
+    
+        return res.status(200).json(tasks);
+    
+    }
+
+    getTaskById = async (req, res) => {
+
+        const taskId = req.params.id;
+    
+        const task = await Task.findById(taskId).exec().catch(err => console.log(err));
+    
+        return res.status(200).json(task);
+    
+    }
+
+    addTask = async (req, res) => {
+
+        const newTask = new Task({
+    
+            name: req.body.name,
+            description: req.body.description,
+            dueDate: req.body.dueDate,
+    
+        });
+    
+        newTask.save()
+        .then(task => res.status(201).json(task))
+        .catch(err => console.log(err));
+    
+    }
+
+    updateTaskById = async (req, res) => {
+
+        const taskId = req.params.id;
+    
+        const updates = {
+            name: req.body.name,
+            description: req.body.description,
+            dueDate: req.body.dueDate
+        }
+    
+        const updateOptions = {new: true};
+    
+        const updatedTask = await Task.findByIdAndUpdate(taskId, updates, updateOptions).catch(err => console.log(err));
+    
+        return res.status(200).json(updatedTask);
+    
+    }
+
+    deleteTaskById = async (req, res) => {
+
+        const taskId = req.params.id;
+
+        const deletedTask = await Task.findByIdAndDelete(taskId).exec().catch(err => console.log(err));
+
+        return res.status(204).json(deletedTask);
+
+    }
+
+}
+
+module.exports = TasksController;
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,73 +1,23 @@
-const List = require('../models/list');
 const Task = require('../models/task');
+
+const TaskController = require('../controllers/tasks');
+const taskController = new TaskController();
 
 module.exports = (app) => {
 
     // get a list of all the tasks.
-    app.route('/tasks').get(async (req, res) => {
-
-        const tasks = await Task.find({}).exec().catch(err => console.log(err));
-
-        return res.status(200).json(tasks);
-
-    });
+    app.get('/tasks', taskController.getAllTasks);
 
     // get a task by id.
-    app.route('/tasks/:id').get(async (req, res) => {
-
-        const taskId = req.params.id;
-
-        const task = await Task.findById(taskId).exec().catch(err => console.log(err));
-
-        return res.status(200).json(task);
-
-    });
+    app.get('/tasks/:id', taskController.getTaskById);
 
     // add a task record
-    app.route('/tasks/add').post(async (req, res) => {
-
-        const newTask = new Task({
-
-            name: req.body.name,
-            description: req.body.description,
-            dueDate: req.body.dueDate,
-
-        });
-
-        newTask.save()
-        .then(task => res.status(201).json(task))
-        .catch(err => console.log(err));
-
-    });
+    app.post('/tasks/add', taskController.addTask);
 
     // update a task by id
-    app.route('/tasks/update/:id').post(async (req, res) => {
-
-        const taskId = req.params.id;
-
-        const updates = {
-            name: req.body.name,
-            description: req.body.description,
-            dueDate: req.body.dueDate
-        }
-
-        const updateOptions = {new: true};
-
-        const updatedTask = await Task.findByIdAndUpdate(taskId, updates, updateOptions).catch(err => console.log(err));
-
-        return res.status(200).json(updatedTask);
-
-    });
+    app.post('/tasks/update/:id', taskController.updateTaskById);
 
     // delete task by id
-    app.route('/tasks/delete/:id').delete(async (req, res) => {
-
-        const taskId = req.params.id;
-
-        const deletedTask = await Task.findByIdAndDelete(taskId).exec().catch(err => console.log(err));
-
-        return res.status(204).json(deletedTask);
-
-    });
+    app.delete('/tasks/delete/:id', taskController.deleteTaskById);
 
 }
